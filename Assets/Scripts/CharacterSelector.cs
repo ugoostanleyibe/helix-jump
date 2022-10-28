3c7251f3699f17c2161cf0b88f5f35586c18b4e3
ugoostanleyibe <ugoostanleyibe@gmail.com>
2022-10-28 14:03:51 +0100

    Add Store Menu

---
+++ Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+	public GameObject[] characters;
+
+	private int selectedCharacterIndex = 0;
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		foreach (var character in characters)
+		{
+			character.SetActive(false);
+		}
+
+		characters[selectedCharacterIndex].SetActive(true);
+	}
+
+	public void ChangeCharacter(int characterIndex)
+	{
+		characters[selectedCharacterIndex].SetActive(false);
+		characters[selectedCharacterIndex = characterIndex].SetActive(true);
+	}
+}
